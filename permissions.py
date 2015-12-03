057499edccb7c580edf6d62228f01c074d6403ad
Michael Young <mike@bitready.io>
2015-12-03 14:45:07 -0500

    Formatting

--- permissions.py
+++ permissions.py
@@ -3,34 +3,40 @@ from django.contrib import admin
 
 
 class DeleteOnly(admin.ModelAdmin):
-# + Overrides
+    # Overrides
     def save_model(self, request, obj, form, change):
         pass
-# + Permissions
+    
+    # Permissions
     def has_add_permission(self, request, obj=None):
         return False
-# + Templates
+    
+    # Templates
     change_form_template = 'admin_tools/view_form.html'
 
 
+
 class DisableDelete(admin.ModelAdmin):
-# + Action Options
+    # Action Options
     def get_actions(self, request):
         actions = super(DisableDelete, self).get_actions(request)
         del actions['delete_selected']
         return actions
-# + Permissions
+        
+    # Permissions
     def has_delete_permission(self, request, obj=None):
         return False
 
 
+
 class ChangeOnly(admin.ModelAdmin):
-# + Action Options
+    # Action Options
     def get_actions(self, request):
         actions = super(ChangeOnly, self).get_actions(request)
         del actions['delete_selected']
         return actions
-# + Permissions
+        
+    # Permissions
     def has_add_permission(self, request, obj=None):
         return False
     def has_delete_permission(self, request, obj=None):
@@ -41,18 +47,21 @@ class ChangeOnly(admin.ModelAdmin):
       ex: readonly_fields = MODEL_NAME._meta.get_all_field_names()
 '''
 class ViewOnly(admin.ModelAdmin):
-# + Action Options
+    # Action Options
     def get_actions(self, request):
         actions = super(ViewOnly, self).get_actions(request)
         del actions['delete_selected']
         return actions
-# + Overrides
+
+    # Overrides
     def save_model(self, request, obj, form, change):
         pass
-# + Permissions
+
+    # Permissions
     def has_add_permission(self, request, obj=None):
         return False
     def has_delete_permission(self, request, obj=None):
         return False
-# + Templates
+
+    # Templates
     change_form_template = 'admin_tools/view_form.html'
